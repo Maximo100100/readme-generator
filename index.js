f2a75d54d4ab03134d7e06c9f46d763efb1c9f05
Maximo100100 <Maximoabahamondes@gmail.com>
2020-07-15 18:38:27 -0700

    done for today

--- index.js
+++ index.js
@@ -1,16 +1,69 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
 
-];
+const promptUser = () => {
+    console.log(`
+==========================================================
+Answer the questions to generate your professional readme.
+==========================================================
+`);
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+              return true;
+            } else {
+              console.log('Please enter a title for your project.');
+              return false;
+            }
+          }
+      },
+      {
+        type: 'confirm',
+        name: 'confirmDes',
+        message: 'Would you like to enter a description of your project',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide a description of your project:',
+        when: ({ confirmDes }) => confirmDes
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'confirm',
+        name: 'another',
+        message: 'Would you like to make another Readme?',
+        default: false
+      }
+    ]);
+  };
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+promptUser()
+    .then(answers => console.log(answers))
 
-// function to initialize program
-function init() {
 
-}
+// // array of questions for user
+// const questions = [
 
-// function call to initialize program
-init();
+// ];
+
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,4 +5,18 @@ function generateMarkdown(data) {
 `;
 }
 
+// this is important for later
+const generateAbout = aboutText => {
+  if (!aboutText) {
+    return '';
+  }
+
+  return `
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
+    </section>
+  `;
+};
+
 module.exports = generateMarkdown;
